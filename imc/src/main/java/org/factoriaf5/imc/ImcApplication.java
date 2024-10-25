ec002f37fbddfc91ece349d644bbe16d86aa6c8d
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-25 16:44:26 +0200

    first commit, estructura entera

---
+++ imc/src/main/java/org/factoriaf5/imc/ImcApplication.java
@@ -0,0 +1,56 @@
+package org.factoriaf5.imc;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImcApplication.class, args);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Ingrese su peso en kilos(con coma):");
+		double peso = sc.nextDouble();
+		System.out.println("Ingrese su altura en metros(con coma):");
+		double altura = sc.nextDouble();
+
+		System.out.println("Tu IMC es de " + imc(peso, altura));
+		System.out.println("Tienes " + calcularIMC(peso, altura));
+		sc.close();
+	}
+
+	public static double imc (double peso, double altura) {
+		double imc = peso / (altura * altura);
+		return imc;
+	}
+
+	public static String calcularIMC (double peso, double altura) {
+		double imc = peso / (altura * altura);
+		if (imc < 16) {
+			return "Delgadez Severa";
+			} else if (imc < 17) {
+				return "Delgadez moderada";
+				} else if (imc < 18.5) {
+					return "Delgadez Leve";
+				}  else if (imc < 25) {
+					return "Peso Normal";
+					} else if (imc < 30) {
+						return "Sobrepeso";
+					}  else if  (imc < 35) {
+						return "Obesidad Leve";
+					} else if (imc < 40){
+						return "Obesidad Moderada";
+					} else if  (imc >= 40) {
+						return "Obesidad Mórbida";
+					} else {
+						return "Error";
+					}
+	}
+
+
+
+
+}
